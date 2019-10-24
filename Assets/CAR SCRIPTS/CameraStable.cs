d964abfed362b7f1e16b2cad769028c0fb73402d
Frank Ryan <rfrank130@yahoo.com>
2019-10-24 19:21:02 -0400

    init

---
+++ Assets/CAR SCRIPTS/CameraStable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject TheCar;
+    public float CarX;
+    public float CarY;
+    public float CarZ;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CarX = TheCar.transform.eulerAngles.x;
+        CarY = TheCar.transform.eulerAngles.y;
+        CarZ = TheCar.transform.eulerAngles.z;
+
+        transform.eulerAngles = new Vector3(CarX - CarX, CarY, CarZ - CarZ);
+
+    }
+}
---
+++ Assets/CAR SCRIPTS/Gun.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float damage = 10f;
+    public float range = 100f;
+
+    public Camera gunCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            Debug.Log("YES");
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gunCam.transform.position, gunCam.transform.forward, out hit, range))
+        {
+
+            Target target = hit.transform.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+    }
+}
